29ac2a01f22cda9b220c81604637a26843eda60d
Scott Minter <scott.minter@homes.com>
2017-01-05 17:03:29 -0500

    work with strings

--- WorkingWithStrings.playground/Contents.swift
+++ WorkingWithStrings.playground/Contents.swift
@@ -3,8 +3,26 @@
 import UIKit
 
 var str = "Hello"
-var newStr = str + ", World!"
+var newStr = str + " Scott!"
 
 for char in newStr.characters {
     print(char)
 }
+
+let newTypeStr: NSString = NSString(string: newStr)
+newTypeStr.substring(to: 5)
+newTypeStr.substring(from: 3)
+
+NSString(string: newTypeStr.substring(from: 6)).substring(to: 5)
+
+newTypeStr.substring(with: NSRange(location: 6, length: 5))
+
+if newTypeStr.contains("Scott") {
+    print("string contains Scott")
+}
+
+let str1 = "Some-Value-Kabob-Cased"
+let str1Arr: [String] = str1.components(separatedBy: "-")
+print(str1Arr)
+
+str1Arr.joined(separator: "_")
